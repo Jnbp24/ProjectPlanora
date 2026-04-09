6d3134dfb9e2137f1f6b7d133e302944978c55ad
Jonas-Petersen <jonasvm6@hotmail.com>
2026-04-09 11:51:11 +0200

    Tasks - unfinished

---
+++ Planora.Api/Services/TaskService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Planora.DTO.TaskDTO;
+using Planora.DataAccess.Repositories.Task;
+
+namespace Planora.Api.Services
+{
+    public class TaskService
+    {
+        public readonly TaskRepository _repo;
+
+        public TaskService(TaskRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<TaskDTO> GetAll() => _repo.;
+
+        public TaskDTO? GetById(int id) => _repo.GetTaskById(id);
+
+        public TaskDTO Create(TaskDTO dto) => _repo.CreateTask(dto);
+
+        public void Update(int id, TaskDTO dto) => _repo.UpdateTask(id, dto);
+
+        public void Delete(int id) => _repo.DeleteTask(id);
+    }
+}
---
+++ Planora.DataAccess/Models/TaskDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planora.DataAccess.Models
+{
+    internal class TaskDB
+    {
+        public TaskDB()
+        {
+        }
+        public Guid TaskID { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+        public bool Deleted { get; set; } = false;
+    }
+}
