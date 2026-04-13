142c538a5c51c6a484737cb1a9ff602074612128
emilnordstroem <emilstoeve@outlook.com>
2026-04-13 12:48:16 +0200

    Changed SaveChangesAsync method

--- Planora.DataAccess/Repositories/IRepository.cs
+++ Planora.DataAccess/Repositories/IRepository.cs
@@ -4,5 +4,5 @@ public interface IRepository<T> where T : class {
     Task<T> CreateAsync(T entity);
     Task<IEnumerable<T>> GetAllAsync();
     Task<T> GetByIdAsync(string id);
-    public System.Threading.Tasks.Task SaveChangesAsync();
+    void SaveChangesAsync();
 }
