e37f684a01876b3d064430fe12454961737f6d3f
emilnordstroem <emilstoeve@outlook.com>
2026-04-13 13:21:18 +0200

    Refactoring Task Service

--- Planora.Api/Services/Task/ITaskService.cs
+++ Planora.Api/Services/Task/ITaskService.cs
@@ -7,9 +7,9 @@ namespace Planora.Api.Services.Task
     public interface ITaskService
     {
         Task<TaskDTO> CreateAsync(TaskDTO dto);
-        System.Threading.Tasks.Task UpdateAsync(string taskId, TaskDTO dto);
+        Task<TaskDTO> UpdateAsync(string taskId, TaskDTO dto);
         Task<IEnumerable<TaskDTO>> GetAllAsync();
         Task<TaskDTO?> GetByIdAsync(string taskId);
-        System.Threading.Tasks.Task DeleteAsync(string taskId);
+        Task<TaskDTO> DeleteAsync(string taskId);
     }
 }
