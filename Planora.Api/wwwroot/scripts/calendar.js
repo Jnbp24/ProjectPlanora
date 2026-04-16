dc393745b0f6d3f9093e3d5f64ab2a73b59333cd
emilnordstroem <emilstoeve@outlook.com>
2026-04-16 09:55:15 +0200

    Select task functionality

--- Planora.Api/wwwroot/scripts/calendar.js
+++ Planora.Api/wwwroot/scripts/calendar.js
@@ -4,8 +4,8 @@ const read_fail = "Failed to read tasks"
 async function setup_calendar() {
     try {
         const data = await get(url, read_fail)
-        const tasks = map_to_task(data)
-        create_calendar(tasks)
+        const events = map_to_event(data)
+        create_calendar(events)
     } catch (error) {
         error_message(error.message)
     }
@@ -27,11 +27,16 @@ async function get(url, error_message) {
     return await response.json();
 }
 
-function map_to_task(data) {
+function map_to_event(data) {
     return data.map(task => ({
         id: task.taskId,
         title: task.title,
-        start: new Date("2026-05-15"),
+        start: task.deadline,
+        extendedProps: {
+            content: task.content,
+            category: task.category
+        },
+        allDay: true
     }));
 }
 /* All tasks in calendar => events must follow this format
@@ -52,10 +57,46 @@ function create_calendar(tasks) {
 
     const calendar = new FullCalendar.Calendar(calendarElement, {
         initialView: 'listMonth',
-        events: tasks
+        events: tasks,
+        eventClick: task_click_handler
     })
 
     calendar.render();
+}
+
+function task_click_handler(info) {
+    const data = info.event
+    const task = map_to_task(data)
+
+    const topbar_element = document.querySelector(".topbar")
+    topbar_element.id = task.id
+
+    const delete_task_btn = document.getElementById("delete_task_btn")
+    delete_task_btn.classList.remove("invisible")
+
+    const newTaskBtn = document.querySelector(".new-task-btn")
+    
+    const titleInput = document.querySelector(".task-name")
+    const contentInput = document.querySelector(".task-content")
+    const categoryInput = document.querySelector(".task-category")
+    const dateInput = document.querySelector(".task-date")
+
+    titleInput.value = task.title
+    contentInput.value = task.content
+    categoryInput.value = task.category
+    if (task.deadline) {
+        dateInput.value = new Date(task.deadline).toISOString().split("T")[0]
+    }
+}
+
+function map_to_task(data) {
+    return {
+        id: data.id,
+        title: data.title,
+        deadline: data.start,
+        content: data.extendedProps.content,
+        category: data.extendedProps.category
+    }
 }
 
 function error_message(message) {
