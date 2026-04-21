e154eeae732b4268e20d5b79af289ca16035b3be
emilnordstroem <emilstoeve@outlook.com>
2026-04-21 09:50:39 +0200

    Restricted delete button

---
+++ Planora.Api/wwwroot/scripts/decode_token.js
@@ -0,0 +1,20 @@
+﻿function is_tovholder() {
+    const token = sessionStorage.getItem("token");
+    if (!token) return false;
+
+    try {
+        const decoded = jwt_decode(token);
+
+        const roles =
+            decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+            || decoded["role"];
+
+        return Array.isArray(roles)
+            ? roles.includes("tovholder")
+            : roles === "tovholder";
+
+    } catch (e) {
+        console.error("Token decode failed:", e);
+        return false;
+    }
+}
