29bf2995f03392b74be807cf60def9704ea2d454
slyng <slyngborgmartin@gmail.com>
2026-04-21 13:05:48 +0200

    finished users gui

--- Planora.Core/UserDTO/UserDTO.cs
+++ Planora.Core/UserDTO/UserDTO.cs
@@ -1,3 +1,3 @@
 ﻿namespace Planora.DTO.UserDTO;
 
-public record UserDTO(string? UserId, string FirstName, string LastName, string Email, bool Tovholder);
+public record UserDTO(string? UserId, string FirstName, string LastName, string? Email, bool Tovholder);
