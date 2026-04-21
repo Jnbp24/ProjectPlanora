450951d5619363edadd4de6965677b6461ea71a4
slyng <slyngborgmartin@gmail.com>
2026-04-21 14:21:29 +0200

    made email for sign up, also added a category.html line

--- Planora.Api/Services/Email/IEmailService.cs
+++ Planora.Api/Services/Email/IEmailService.cs
@@ -3,4 +3,6 @@
 public interface IEmailService
 {
     System.Threading.Tasks.Task SendPasswordResetEmail(string toEmail, string resetToken);
+    System.Threading.Tasks.Task SendSignUpEmail(string toEmail, string resetToken);
+
 }
